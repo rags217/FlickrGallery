f6f930555f4cb643ed84e27d8f52a16e7ad8e302
rags217 <Tamanna123>
2019-02-13 16:54:22 +0000

    Showing a simple list from Flickr

--- app/src/main/java/com/rags/flickrgallery/Item.java
+++
@@ -1,5 +0,0 @@
-package com.rags.flickrgallery;
-
-public class Item {
-
-}
---
+++ app/src/main/java/com/rags/flickrgallery/com/rags/flickrgallery/api/ListDownloadTask.java
@@ -0,0 +1,39 @@
+package com.rags.flickrgallery.com.rags.flickrgallery.api;
+
+import android.os.AsyncTask;
+
+import com.rags.flickrgallery.HomeView;
+import com.rags.flickrgallery.model.Item;
+
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+
+public class ListDownloadTask extends AsyncTask<Void, Integer, FlickrResponse> {
+
+    private HomeView view;
+
+    public ListDownloadTask(HomeView view) {
+        this.view = view;
+    }
+
+    @Override
+    protected FlickrResponse doInBackground(Void... v) {
+        return FlickrController.downloadTrainTimes(new OkHttpClient());
+
+    }
+
+    @Override
+    protected void onPostExecute(FlickrResponse response) {
+
+        if(response.hasItemsResponse()) {
+
+            List<Item> items = response.getItems();
+            if (items.size() > 0)
+                view.showList(items);
+            else
+                view.showNothing();
+        } else
+            view.showError(response.getErrorMessageResId());
+    }
+}
